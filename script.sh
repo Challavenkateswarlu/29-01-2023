6038e0671a7ee70d8ab97f62ac6a047caa437aad
venkatesh <challa@gmail.com>
2024-01-29 06:52:19 +0000

    added

--- script.sh
+++ script.sh
@@ -19,6 +19,9 @@ read sal
 if [ $sal -lt 15000 ];
 then
         echo "no tax"
+elif [ $sal -eq 15000 ];
+then
+	echo " taxis 2%"
 elif [ $sal -gt 15000 -a $sal -le 30000 ];
 then 
         echo " tax is 5%"
